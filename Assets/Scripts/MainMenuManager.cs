9dcea116d21f2db6acbda2259ec4de94c9afda30
yanjingzz <yanjingzz@gmail.com>
2018-11-12 22:06:22 -0800

    Added load screen

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -4,8 +4,22 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour {
+    public GameObject MainMenu;
+    public GameObject Loading;
 
     public void PlayGame() {
-        SceneManager.LoadScene(1);
+        MainMenu.SetActive(false);
+        Loading.SetActive(true);
+        StartCoroutine(AsyncLoadScene());
     }
+
+    IEnumerator AsyncLoadScene() {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(1);
+        // Wait until the asynchronous scene fully loads
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+    }
+
 }
